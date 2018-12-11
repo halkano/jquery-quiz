bd96f43d8b867bb24885c813115cb35b3e503f04
henry halkano <halkanoh10@gmail.com>
2018-12-11 08:53:28 +0300

    updating my jquery and index page

--- js/script.js
+++ js/script.js
@@ -1,37 +1,99 @@
-function check(){
-var q1=document.myform.q1.value;
-var q2=document.myform.q2.value;
-var q3=document.myform.q3.value;
-  var q4=document.myform.q4.value;
-    var q5=document.myform.q5.value;
-    var q6=document.myform.q6.value;
-      var q7=document.myform.q7.value;
-        var q8=document.myform.q8.value;
-var count=0;
+// Back-end logic
+var inputedAnswers = [],
+    scoredPoints = 0;
+    questions = ["ans-1", "ans-2", "ans-3", "ans-4"];
 
-if (q1=="b"){
-          count++;
+function pointsCompiler(){
+  scoredPoints = inputedAnswers[0] + inputedAnswers[1] + inputedAnswers[2] + inputedAnswers[3];
+  return points;
 }
-if (q2=="b"){
-          count++;
-}
-if (q3=="b"){
-          count++;
-}
-if (q4=="a"){
-          count++;
-}
-if (q5=="a"){
-          count++;
-}
-if (q6=="d"){
-          count++;
-}
-if (q7=="b"){
-          count++;
-}
-if (q8=="a"){
-          count++;
-}
-alert("YOU GOT: "+count+" OUT OF 8 MARKS");
-}
+// Front-end logic
+$(document).ready(function(){
+  $(".question").hide();
+  $("#q-1").show();
+
+  $("#btn-1").click(function(){
+    if(isNaN($("input:radio[name = ans-1]:checked").val())){
+      alert("Please answer question 1 to proceed!");
+      $(".question").hide();
+      $("#q-1").show();
+      return false;
+    }else{
+      $(".question").hide();
+      $("#q-1").hide();
+      $("#q-2").show();
+      return false;
+    }
+  });
+  $("#btn-2-1").click(function(){
+   $(".question").hide();
+   $("#q-2").hide();
+   $("#q-1").show();
+   return false;
+ });
+ $("#btn-2-2").click(function(){
+   if(isNaN($("input:radio[name = ans-2]:checked").val())){
+     alert("Please answer question 2 to proceed!");
+     $(".question").hide();
+     $("#q-1").hide();
+     $("#q-2").show();
+     return false;
+   }else{
+     $(".question").hide();
+     $("#q-2").hide();
+     $("#q-3").show();
+     return false;
+   }
+ });
+
+ $("#btn-3-1").click(function(){
+   $(".question").hide();
+   $("#q-3").hide();
+   $("#q-2").show();
+   return false;
+ });
+ $("#btn-3-2").click(function(){
+   if(isNaN($("input:radio[name = ans-3]:checked").val())){
+     alert("Please answer question 3 to proceed!");
+     $(".question").hide();
+     $("#q-2").hide();
+     $("#q-3").show();
+     return false;
+   }else{
+     $(".question").hide();
+     $("#q-3").hide();
+     $("#q-4").show();
+     return false;
+   }
+ });
+
+ $("#btn-4-1").click(function(){
+   $(".question").hide();
+   $("#q-4").hide();
+   $("#q-3").show();
+   return false;
+ });
+
+ $("#btn-4-2").click(function(){
+   if(isNaN($("input:radio[name = ans-4]:checked").val())){
+     alert("Please answer question 4 to submit your answers!");
+     $(".question").hide();
+     $("#q-3").hide();
+     $("#q-4").show();
+     return false;
+   }else{
+     $("#fm-1").submit(function(event){
+       event.preventDefault();
+       questions.forEach(function(question){
+         inputedAnswers.push(parseInt($("input:radio[name=" + question + "]:checked").val()));
+       });
+       pointsCompiler();
+
+       $("#test").hide();
+       $("#q-4").hide();
+       $("#output").show();
+       $("#points").append(scoredPoints);
+     });
+   }
+ });
+});
